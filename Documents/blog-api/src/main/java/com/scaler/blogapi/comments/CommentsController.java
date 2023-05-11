24012736bdf75559401971be72818a82291f201e
akhil-lakka <akhillakka@gmail.com>
2023-05-11 08:16:37 +0530

    initialize empty controller for h2 database

---
+++ Documents/blog-api/src/main/java/com/scaler/blogapi/comments/CommentsController.java
@@ -0,0 +1,10 @@
+package com.scaler.blogapi.comments;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/blogs/{blogID}/comments")
+public class CommentsController {
+
+}
